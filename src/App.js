77f6aaa5f6fb109ba9b90859b59018c375198f7e
Abdelhadi <109425691+AbdelhadiCpu@users.noreply.github.com>
2022-07-19 12:12:41 +0100

    first commit
    first

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Route, Routes } from 'react-router-dom';
+//import LogIn from './components/Login/login';
+//import Menu1 from './components/Menu/menu1';
+import LogIn from './components/Login/login';
+import SignUp from './components/SingUp/signup';
+import Menu2 from './components/Menu/menu2';
+import ReSet from './components/Reset/reset';
+import Page1 from './components/page1/page1';
+import Search from './components/Search/search';
+
+
+
+
+
+function App(){
+  return(
+
+   <>
+
+    <Routes>
+    <Route  exact path='/' element={<LogIn/>}/>
+    <Route exact path = '/home' element={<Menu2/>}/>
+    <Route  exact path = '/register' element={<SignUp/>}/>
+    <Route exact path='/reset' element={<ReSet />} />
+    <Route exact path ='/firstpage' element={<Page1/>}/>
+    <Route exact path ='/home/search' element={<Search/>}/>
+   
+    </Routes>
+
+  
+
+   </>
+    
+
+  );
+}
+export default App
